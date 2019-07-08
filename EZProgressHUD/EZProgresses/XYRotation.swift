9fadaa1d08f59cff55431476cc041c8539c72821
NP2 <sahandraeisi1994@gmail.com>
2019-07-08 13:27:08 +0430

    Added XYRotation and YRotation Progresses

---
+++ EZProgressHUD/EZProgresses/XYRotation.swift
@@ -0,0 +1,72 @@
+//
+//  XYRotation.swift
+//  EZProgressHUD
+//
+//  Created by NP2 on 7/8/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final public class XYRotation: EZBaseView {}
+
+// MARK: EZProgressProtocol Impelementation
+
+extension XYRotation: EZProgress {
+    
+    public func show() {
+        
+        setLayers()
+        UIApplication.shared.keyWindow?.isUserInteractionEnabled = false
+        UIApplication.shared.keyWindow?.addSubview(transView)
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseInOut, animations: {
+            self.transView.alpha = 1
+            self.transView.layoutIfNeeded()
+        })
+    }
+    
+    public func dismiss(completion: (() -> Void)?) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseInOut, animations: {
+            
+            self.transView.alpha = 0
+            self.transView.layoutIfNeeded()
+        }) { _ in
+            
+            self.transView.removeFromSuperview()
+            UIApplication.shared.keyWindow?.isUserInteractionEnabled = true
+            completion?()
+        }
+    }
+}
+
+// MARK: Set Animation And Layers
+
+extension XYRotation: EZProgressAnimation {
+    
+    public func setLayers() {
+        transView.alpha = 0
+        titleLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 70)
+        titleLabel.center = transView.center
+        transView.addSubview(titleLabel)
+        layerGenerator(shapeLayer: secondShapeLayer, type: .secondShapeLayer)
+        layerGenerator(shapeLayer: thirdShapeLayer, type: .thirdShapeLayer)
+        layerGenerator(shapeLayer: firstShapeLayer, type: .firstShapeLayer)
+        setAnimation()
+    }
+    
+    public func setAnimation() {
+        
+        let rotationXAnimation = EZAnimations.rotationX(duration: 6)
+        let rotationYAnimation = EZAnimations.rotationY(duration: 3)
+        let transformScaleAnimation = EZAnimations.transform(toValue: 1.06, duration: 1.0, option: .easeInEaseOut)
+        let opacityAnimation = EZAnimations.opacity()
+        
+        firstShapeLayer.add(transformScaleAnimation, forKey: EZStrings.hsKey0.rawValue)
+        secondShapeLayer.add(rotationXAnimation, forKey: EZStrings.hsKey1.rawValue)
+        thirdShapeLayer.add(rotationYAnimation, forKey: EZStrings.hsKey2.rawValue)
+        titleLabel.layer.add(opacityAnimation, forKey: EZStrings.hsKey3.rawValue)
+    }
+}
+
