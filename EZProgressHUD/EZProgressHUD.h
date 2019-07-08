a04f4462925d4aefb99fb38a41835c36dc0ea11a
NP2 <sahandraeisi1994@gmail.com>
2019-07-08 12:06:14 +0430

    Initial Commit

---
+++ EZProgressHUD/EZProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  EZProgressHUD.h
+//  EZProgressHUD
+//
+//  Created by NP2 on 7/8/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EZProgressHUD.
+FOUNDATION_EXPORT double EZProgressHUDVersionNumber;
+
+//! Project version string for EZProgressHUD.
+FOUNDATION_EXPORT const unsigned char EZProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZProgressHUD/PublicHeader.h>
+
+
---
+++ EZProgressHUDExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EZProgressHUDExamples
+//
+//  Created by NP2 on 7/8/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
