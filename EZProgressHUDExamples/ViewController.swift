7e6ea6f7f8033a98bc7b305477264dfa0be0cc7f
NP2 <sahandraeisi1994@gmail.com>
2019-07-09 14:18:19 +0430

    Added heartBeat indicator example

--- EZProgressHUDExamples/ViewController.swift
+++ EZProgressHUDExamples/ViewController.swift
@@ -11,5 +11,26 @@ import EZProgressHUD
 
 final class ViewController: UIViewController {
     
+    @IBAction private func heartBeatButtonPressed(_ sender: UIButton) {
+        
+        let progress = EZProgressOptions { (options) in
+            options.radius = 120
+            options.secondLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+            options.strokeWidth = 12
+            options.thirdLayerStrokeColor = .gray
+            options.firstLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+            options.title = "shndrs presents"
+            options.animationOption = EZAnimationOptions.heartBeat
+        }
+        
+        let hud = EZProgressHUD.set(progress: progress)
+        hud.show()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            hud.dismiss(completion: nil)
+        }
+    }
+    
+    
     
 }
