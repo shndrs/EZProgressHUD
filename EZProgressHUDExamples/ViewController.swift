3f05bafdcf8f58605792b869e624600eeb3b2288
NP2 <sahandraeisi1994@gmail.com>
2019-07-09 14:38:20 +0430

    Added LordOfTheRings and XRotation example

--- EZProgressHUDExamples/ViewController.swift
+++ EZProgressHUDExamples/ViewController.swift
@@ -15,17 +15,18 @@ final class ViewController: UIViewController {
     
     @IBAction private func heartBeatButtonPressed(_ sender: UIButton) {
         
-        let progress = EZProgressOptions { (options) in
-            options.radius = 120
-            options.secondLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
-            options.strokeWidth = 12
-            options.thirdLayerStrokeColor = .gray
-            options.firstLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
-            options.title = "shndrs presents"
-            options.animationOption = EZAnimationOptions.heartBeat
+        let options = EZProgressOptions { (option) in
+            option.radius = 120
+            option.secondLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+            option.strokeWidth = 12
+            option.thirdLayerStrokeColor = .gray
+            option.firstLayerStrokeColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+            option.title = "shndrs presents"
+            option.animationOption = EZAnimationOptions.heartBeat
         }
         
-        let hud = EZProgressHUD.set(progress: progress)
+        let hud = EZProgressHUD.setProgress(with: options)
+        
         hud.show()
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
@@ -37,22 +38,65 @@ final class ViewController: UIViewController {
     
     @IBAction private func lineLayerButtonPressed(_ sender: UIButton) {
         
-        let progress = EZProgressOptions { (options) in
-            options.radius = 120
-            options.secondLayerStrokeColor = UIColor(red: 255/255, green: 190/255, blue: 0/255, alpha: 1)
-            options.strokeWidth = 13
-            options.thirdLayerStrokeColor = UIColor(red: 255/255, green: 190/255, blue: 0/255, alpha: 1)
-            options.firstLayerStrokeColor = .black
-            options.title = "shndrs presents"
-            options.animationOption = EZAnimationOptions.lineLayer
+        let options = EZProgressOptions { (option) in
+            option.radius = 120
+            option.secondLayerStrokeColor = UIColor(red: 255/255, green: 190/255, blue: 0/255, alpha: 1)
+            option.strokeWidth = 13
+            option.thirdLayerStrokeColor = UIColor(red: 255/255, green: 190/255, blue: 0/255, alpha: 1)
+            option.firstLayerStrokeColor = .black
+            option.title = "shndrs presents"
+            option.animationOption = EZAnimationOptions.lineLayer
         }
         
-        let hud = EZProgressHUD.set(progress: progress)
+        let hud = EZProgressHUD.setProgress(with: options)
         hud.show()
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
             hud.dismiss(completion: nil)
         }
+    }
     
+    // MARK: - LordOfTheRings HUD
     
+    @IBAction private func lordOfTheRingsButtonPressed(_ sender: UIButton) {
+        
+        let options = EZProgressOptions { (option) in
+            option.radius = 120
+            option.secondLayerStrokeColor = .white
+            option.strokeWidth = 12
+            option.thirdLayerStrokeColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+            option.firstLayerStrokeColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1.0)
+            option.title = "shndrs presents"
+            option.animationOption = EZAnimationOptions.lordOfTheRings
+        }
+        
+        let hud = EZProgressHUD.setProgress(with: options)
+        hud.show()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            hud.dismiss(completion: nil)
+        }
+    }
+    
+    // MARK: - XRotation HUD
+    
+    @IBAction private func xRotationButtonPressed(_ sender: UIButton) {
+        
+        let options = EZProgressOptions { (option) in
+            option.radius = 100
+            option.secondLayerStrokeColor = .brown
+            option.strokeWidth = 12
+            option.thirdLayerStrokeColor = .gray
+            option.firstLayerStrokeColor = .darkGray
+            option.title = "shndrs presents"
+            option.animationOption = EZAnimationOptions.xRotation
+        }
+        
+        let hud = EZProgressHUD.setProgress(with: options)
+        hud.show()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            hud.dismiss(completion: nil)
+        }
+    }
 }
