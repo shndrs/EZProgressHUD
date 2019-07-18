230aa60a37ad9d1f399ffb94335165f2df17c891
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-07-18 22:21:55 +0430

    Added titleOptions class

--- EZProgressHUD/EZProgressOptions/EZProgressOptions.swift
+++ EZProgressHUD/EZProgressOptions/EZProgressOptions.swift
@@ -21,11 +21,24 @@ public enum EZAnimationOptions {
 }
 
 @requires_stored_property_inits
-public class EZProgressColorOptions {
-    
-    private static let red = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+public class titleOptions {
     /// the stroke width of circles (by default is 12.0)
     public var strokeWidth: CGFloat = 12.0
+    
+    /// title text (by default is Please Wait...)
+    public var title: String = EZStrings.pleaseWait.rawValue
+    
+    /// font of title text (by default is Papyrus size 18)
+    public var font: UIFont = UIFont(name: EZStrings.fontName.rawValue, size: 18)!
+    
+    /// color of title text (by default is white)
+    public var titleTextColor: UIColor = .white
+}
+
+@requires_stored_property_inits
+public class EZProgressColorOptions: titleOptions {
+    
+    private static let red = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
     
     /// the color of main circle (default is red)
     public var firstLayerStrokeColor: UIColor = red
@@ -40,17 +53,8 @@ public class EZProgressColorOptions {
     public var transViewBackgroundColor: UIColor = UIColor.black
 }
 
-@requires_stored_property_inits public
-class EZProgressOptions: EZProgressColorOptions {
-    
-    /// title text (by default is Please Wait...)
-    public var title: String = EZStrings.pleaseWait.rawValue
-    
-    /// font of title text (by default is Papyrus size 18)
-    public var font: UIFont = UIFont(name: EZStrings.fontName.rawValue, size: 18)!
-    
-    /// color of title text (by default is white)
-    public var titleTextColor: UIColor = .white
+@requires_stored_property_inits
+public class EZProgressOptions: EZProgressColorOptions {
     
     /// the radius of the circles (by default is 115.0)
     public var radius: CGFloat = 115.0
