1e090234bcd29e1967182c338b5234632a44697a
NP2 <sahandraeisi1994@gmail.com>
2019-07-24 14:39:58 +0430

    Put animations in individual methods in YRotation

--- EZProgressHUD/EZProgresses/YRotation.swift
+++ EZProgressHUD/EZProgresses/YRotation.swift
@@ -10,17 +10,34 @@ import UIKit
 
 final public class YRotation: EZBaseProgresses {
     public override func setAnimation() {
-        let rotationYAnimation = EZAnimations.rotation(duration: 4,
-                                                       rotationMode: .yRotation)
-        let innerPulsateArgs = TransformArguments(toValue: 1.1, duration: 0.8, option: .easeOut)
-        let transformAnimationInnerPulsate = EZAnimations.transform(with: innerPulsateArgs)
-        let opacityAnimation = EZAnimations.opacity()
+        transformScaleAnimations()
+        rortaionYAnimation()
+        opacityAnimation()
+    }
+}
+
+// MARK: - Methods
+
+extension YRotation {
+    
+    private func transformScaleAnimations() {
         let transformScaleArgs = TransformArguments(toValue: 1.05, duration: 1.0, option: .easeInEaseOut)
         let transformScaleAnimation = EZAnimations.transform(with: transformScaleArgs)
+        let innerPulsateArgs = TransformArguments(toValue: 1.1, duration: 0.8, option: .easeOut)
+        let transformAnimationInnerPulsate = EZAnimations.transform(with: innerPulsateArgs)
         firstShapeLayer.add(transformScaleAnimation, forKey: EZStrings.hsKey0.rawValue)
-        secondShapeLayer.add(rotationYAnimation, forKey: EZStrings.hsKey1.rawValue)
-        thirdShapeLayer.add(transformAnimationInnerPulsate, forKey: EZStrings.hsKey2.rawValue)
-        titleLabel.layer.add(opacityAnimation, forKey: EZStrings.hsKey3.rawValue)
         titleLabel.layer.add(transformScaleAnimation, forKey: EZStrings.hsKey4.rawValue)
+        thirdShapeLayer.add(transformAnimationInnerPulsate, forKey: EZStrings.hsKey2.rawValue)
+    }
+    
+    private func rortaionYAnimation() {
+        let rotationYAnimation = EZAnimations.rotation(duration: 4,
+                                                       rotationMode: .yRotation)
+        secondShapeLayer.add(rotationYAnimation, forKey: EZStrings.hsKey1.rawValue)
+    }
+    
+    private func opacityAnimation() {
+        let opacityAnimation = EZAnimations.opacity()
+        titleLabel.layer.add(opacityAnimation, forKey: EZStrings.hsKey3.rawValue)
     }
 }
