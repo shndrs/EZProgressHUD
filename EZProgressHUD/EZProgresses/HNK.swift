25bf43f7bde3bcaf9dfbc5e7e6f2fe373fcb7431
NP2 <sahandraeisi1994@gmail.com>
2019-07-24 14:25:01 +0430

    Put animations in individual methods in HNK

--- EZProgressHUD/EZProgresses/HNK.swift
+++ EZProgressHUD/EZProgresses/HNK.swift
@@ -22,28 +22,45 @@ final public class HNK: EZBaseProgresses {
     
     public override func setAnimation() {
         setProperties()
+        rotationAnimations()
+        scaleAnimations()
+        dashAnimations()
+        strokeEndAndTitleAnimations()
+    }
+    
+    private func rotationAnimations() {
         let rotationYAnimation = EZAnimations.rotation(duration: 6,
                                                        rotationMode: .yRotation)
+        secondShapeLayer.add(rotationYAnimation, forKey: EZStrings.hsKey1.rawValue)
+    }
+    
+    private func scaleAnimations() {
         let transformScaleArgs = TransformArguments(toValue: 0.96, duration: 2.0,
                                                     option: .easeInEaseOut)
         let transformScaleAnimation = EZAnimations.transform(with: transformScaleArgs)
         let transformScaleArgs3 = TransformArguments(fromValue: 1.04, toValue: 1.09,
                                                      duration: 4, option: .easeInEaseOut)
         let transformScaleAnimation3 = EZAnimations.transform(with: transformScaleArgs3)
-        let opacityAnimation = EZAnimations.opacity()
+        firstShapeLayer.add(transformScaleAnimation, forKey: EZStrings.hsKey0.rawValue)
+        thirdShapeLayer.add(transformScaleAnimation3, forKey: EZStrings.hsKey2.rawValue)
+    }
+    
+    private func dashAnimations() {
         let lineDashArgs = LineDashPhaseArguments(byValue: 18.0, duration: 0.75,
                                                   option: .linear)
         let lineDashPhaseAnimation = EZAnimations.lineDashPhase(with: lineDashArgs)
-        let strokeEndAnimation = EZAnimations.strokeEndAnimation(toValue: 1,
-                                                                 duration: 0.4,
-                                                                 option: .easeIn)
-        firstShapeLayer.add(transformScaleAnimation, forKey: EZStrings.hsKey0.rawValue)
-        secondShapeLayer.add(rotationYAnimation, forKey: EZStrings.hsKey1.rawValue)
-        thirdShapeLayer.add(transformScaleAnimation3, forKey: EZStrings.hsKey2.rawValue)
-        titleLabel.layer.add(opacityAnimation, forKey: EZStrings.hsKey3.rawValue)
         firstShapeLayer.add(lineDashPhaseAnimation, forKey: EZStrings.hsKey4.rawValue)
         secondShapeLayer.add(lineDashPhaseAnimation, forKey: EZStrings.hsKey5.rawValue)
         thirdShapeLayer.add(lineDashPhaseAnimation, forKey: EZStrings.hsKey6.rawValue)
+    }
+    
+    private func strokeEndAndTitleAnimations() {
+        let opacityAnimation = EZAnimations.opacity()
+        
+        let strokeEndAnimation = EZAnimations.strokeEndAnimation(toValue: 1,
+                                                                 duration: 0.4,
+                                                                 option: .easeIn)
+        titleLabel.layer.add(opacityAnimation, forKey: EZStrings.hsKey3.rawValue)
         firstShapeLayer.add(strokeEndAnimation, forKey: EZStrings.hsKey7.rawValue)
     }
 }
