d8d88cdecab69a08b474f20f5b58f6f99209c0c1
NP2 <sahandraeisi1994@gmail.com>
2019-07-24 14:44:04 +0430

    Put animations in individual methods in HeartBeat

--- EZProgressHUD/EZProgresses/HeartBeat.swift
+++ EZProgressHUD/EZProgresses/HeartBeat.swift
@@ -10,17 +10,30 @@ import UIKit
 
 final public class HeartBeat: EZBaseProgresses {
     public override func setAnimation() {
+        transformAnimations()
+        titleAnimations()
+    }
+}
+
+// MARK: -  Methods
+
+extension HeartBeat {
+    
+    private func transformAnimations() {
         let transformAnimationObj = TransformArguments(toValue: 1.07, duration: 0.8, option: .easeOut)
         let transformAnimation = EZAnimations.transform(with: transformAnimationObj)
         let innerPulsateObj = TransformArguments(toValue: 0.95, duration: 1.6, option: .easeOut)
         let transformAnimationInnerPulsate = EZAnimations.transform(with: innerPulsateObj)
-        let opacityAnimation = EZAnimations.opacity()
-        let transformXScaleAnimation = EZAnimations.transformX()
-
         secondShapeLayer.add(transformAnimation,
                              forKey: EZStrings.hsKey0.rawValue)
         thirdShapeLayer.add(transformAnimationInnerPulsate,
                             forKey: EZStrings.hsKey1.rawValue)
+    }
+    
+    private func titleAnimations() {
+        let opacityAnimation = EZAnimations.opacity()
+        let transformXScaleAnimation = EZAnimations.transformX()
+        
         titleLabel.layer.add(opacityAnimation,
                              forKey: EZStrings.hsKey2.rawValue)
         titleLabel.layer.add(transformXScaleAnimation,
